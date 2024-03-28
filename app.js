b98a992dc20698cd3da0d5c3a03628d8d57dd86a
Pravin A <a.pravin3210@gmail.com>
2024-03-28 12:33:28 +0530

    91 Adding place-specific routes

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const bodyParser=require('body-parser')
 const placeRouter=require('./routes/place-router')
 const app=express();
 
-app.use(placeRouter)  //Now we have added the route as a middleware in app.js
+app.use('/api/places', placeRouter)  //Now we have added the route as a middleware in app.js . By giving /api/places in 1st argument we filter the routes according to this
 
 app.listen(5000)
--- routes/place-router.js
+++ routes/place-router.js
@@ -2,9 +2,27 @@ const express=require('express')
 
 const router=express.Router()
 
-router.get('/',(req,res)=>{
-    console.log('Get request in places')
-    res.json({message:'It works'})
+const DUMMY_PLACES= [{
+    id: 'p1',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world!',
+    imageUrl:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+    address: '20 W 34th St, New York, NY 10001',
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584
+    },
+    creator: 'u1'
+  }]
+router.get('/:pid',(req,res)=>{
+    const placeID=req.params.pid
+    console.log('id',placeID)
+    const place=DUMMY_PLACES.find(p=>{
+        return p.id===placeID
+    })
+    console.log('place',place)
+   res.json({place}) 
 })
 
 module.exports=router
