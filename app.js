190d2dfd6935efc6aa9bfd6b10adf8a7248c639e
Pravin A <a.pravin3210@gmail.com>
2024-04-22 16:42:06 +0530

    97 Handling error for unsupported routes

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express=require('express')
 const bodyParser=require('body-parser')
+const HttpError=require('./models/http-error')
+
 
 const placeRouter=require('./routes/place-router')
 const app=express();
@@ -7,6 +9,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/api/places', placeRouter)  //Now we have added the route as a middleware in app.js . By giving /api/places in 1st argument we filter the routes according to this
+
+
+app.use((req,res, next)=>{
+    const error=new HttpError('Could not find this route', 404)
+    throw error
+})
 
 app.use((error, req,res,next)=>{
     console.log('check',error)
