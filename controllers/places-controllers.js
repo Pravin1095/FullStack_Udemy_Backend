6d40973042def64149f915726b4cbe92cafd592a
Pravin A <a.pravin3210@gmail.com>
2024-10-04 00:19:32 +0530

    #Updating and deleting places using mongoose

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -122,31 +122,52 @@ return next(error) // this should be present else it won't stop executing and co
     res.status(201).json({place:createdPlace})
 }
 
-const updatePlace=(req, res, next)=>{
+const updatePlace=async (req, res, next)=>{
     const errors=validationResult(req)
     if(!errors.isEmpty()){
         console.log('error', errors)
         throw new HttpError('Invalid inputs passed . Please check your data', 422)
     }
-    const {title, description}=req.body
+    const {title, description, address}=req.body
     const id=req.params.pid
-    const updatePlace={...DUMMY_PLACES.find(p=>p.id===id)}
-    const placeIndex=DUMMY_PLACES.findIndex(p=>p.id===id)
-    updatePlace.title=title
-    updatePlace.description=description
-    DUMMY_PLACES[placeIndex]=updatePlace
+    // const updatePlace={...DUMMY_PLACES.find(p=>p.id===id)}
+    // const placeIndex=DUMMY_PLACES.findIndex(p=>p.id===id)
+    // updatePlace.title=title
+    // updatePlace.description=description
+    // DUMMY_PLACES[placeIndex]=updatePlace
 
-    res.status(200).json({place:updatePlace})
+    let place
+    try{
+        place= await Place.findById(id)
+        place.title=title
+        place.description=description
+        place.address=address
+        await place.save()
+    }
+    catch(err){
+        const error=HttpError('Could not update the place details', 500)
+        return next(error)
+    }
+
+    res.status(200).json({place:place.toObject({getters: true})})
 
 
 }
 
-const deletePlace=(req,res,next)=>{
+const deletePlace=async (req,res,next)=>{
     const pid=req.params.pid
-    if(!DUMMY_PLACES.filter(p=>p.id!==pid)){
-        throw new HttpError('Could not find that place for that id', 404);
+    // if(!DUMMY_PLACES.filter(p=>p.id!==pid)){
+    //     throw new HttpError('Could not find that place for that id', 404);
+    // }
+    // DUMMY_PLACES=DUMMY_PLACES.filter(p=>p.id!==pid)
+    let places
+    try{
+        places=await Place.findByIdAndDelete(pid)
     }
-    DUMMY_PLACES=DUMMY_PLACES.filter(p=>p.id!==pid)
+    catch(err){
+const error=new HttpError('Could not delete places', 500)
+return next(error)
+    }
     res.json({message:'Deleted place successfully!'})
 
 }
