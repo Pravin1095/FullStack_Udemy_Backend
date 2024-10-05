fdec5aa091068fb3d3333a4d22ffbe8622a0affe
Pravin A <a.pravin3210@gmail.com>
2024-10-05 17:22:52 +0530

    #Signup mehod with mongoose

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -1,6 +1,7 @@
 const { v4: uuidv4 } = require('uuid');
 const HttpError=require('../models/http-error')
 const {validationResult} = require('express-validator')
+const User=require('../models/user')
 
 let DUMMY_USERS=[
     {
@@ -15,27 +16,54 @@ const getUserName=(req, res, next)=>{
     res.status(200).json({users: DUMMY_USERS})
 }
 
-const signup=(req, res, next)=>{
-    const {name , email ,password}=req.body
+const signup=async (req, res, next)=>{
+    const {name , email ,password, places}=req.body
     const errors=validationResult(req)
     if(!errors.isEmpty()){
         console.log('error', errors)
-        throw new HttpError('Invalid inputs passed . Please check your data', 422)
+        return next(new HttpError('Invalid inputs passed . Please check your data', 422))
     }
-    const hasUser=DUMMY_USERS.find(data=>data.email===email)
-    if(hasUser){
-        throw new HttpError('Could not create user. Email already exists', 422)
-    }
-    const updatedUser={
-        id:uuidv4(),
-        name:name,
-        email:email,
-        password:password
-    }
+    // const hasUser=DUMMY_USERS.find(data=>data.email===email)
+    // if(hasUser){
+    //     throw new HttpError('Could not create user. Email already exists', 422)
+    // }
+    // const updatedUser={
+    //     id:uuidv4(),
+    //     name:name,
+    //     email:email,
+    //     password:password
+    // }
     
-    DUMMY_USERS.push(updatedUser)
-    console.log('in signup', DUMMY_USERS)
-    res.status(201).json({user : updatedUser})
+    // DUMMY_USERS.push(updatedUser)
+    // console.log('in signup', DUMMY_USERS)
+
+    let existingUser
+    let createdUser
+    try{
+    existingUser=await User.findOne({email : email})
+    
+    
+
+    if(existingUser){
+        const error=new HttpError('User already exists. Please login instead', 422)
+        return next(error)
+    }
+
+    createdUser=new User({
+name, 
+email,
+image: 'dummy url',
+password,
+places
+    })
+    await createdUser.save()
+}
+
+    catch(err){
+const error = new HttpError('Signing up failed. please try again', 500)
+return next(err)
+    }
+    res.status(201).json({user : createdUser.toObject({getters: true})})
 }
 
 const login=(req, res, next)=>{
--- models/user.js
+++ models/user.js
@@ -1,16 +1,16 @@
 const mongoose=require('mongoose')
-const uniqueValidator=require('mongoose-unique-validator')
+const uniqueValidator=require('@ladjs/mongoose-unique-validator')
 
 const Schema=mongoose.Schema
 
 const userSchema=new Schema({
     name : {type: String, required: true},
-    email : {type: String, required: true, unique : true},  //the unique property sets index for each email and queries it faster compared to other properties. To achieve this we need to install mongoose-unique-validator and add this as a plugin so our emails will be queried faster
+    email : {type: String, required: true},  //the unique property sets index for each email and queries it faster compared to other properties. To achieve this we need to install mongoose-unique-validator and add this as a plugin so our emails will be queried faster
     password: {type: String, required: true, minlength :6},
     image: {type: String, required: true},
     places: {type: String, required: true},
 })
 
-userSchema.plugin(uniqueValidator)
+// userSchema.plugin(uniqueValidator) //also this unique validator makes sure tht no email is being repeated
 
 module.exports=mongoose.model('User', userSchema)
